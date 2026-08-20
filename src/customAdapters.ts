9e75d3c09eb7595c992b999eb548f6c74fd2b3d5
kanv <kanv@euler.xyz>
2026-08-20 20:45:50 +0000

    update hash

--- src/customAdapters.ts
+++ src/customAdapters.ts
@@ -3,7 +3,7 @@ import { Address } from "viem";
 export const POPPIE_EULER_ADAPTER_CONTRACT_NAME = "PoppieEulerAdapter";
 export const POPPIE_CUSTOM_ADAPTER_DISPLAY_NAME = "Poppie Custom Adaptor";
 export const POPPIE_EULER_ADAPTER_METADATA_HASH =
-  "0x12201a101ae06de263c4508922a47b3a3cbfaf700bf7950cc2d5c57b827174f21c97";
+  "0x1220fc8e0ecc29a1ce0659055e968dfae362c42c436e53586f6c311d3f327018c513";
 
 export type PoppieEulerAdapter = {
   address: Address;
