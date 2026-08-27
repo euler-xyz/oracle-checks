f07845c965fe21c9663bffbaa5da326857c9782f
kanv <kanv@euler.xyz>
2026-08-27 21:23:23 +0530

    change poppie name

--- src/customAdapters.ts
+++ src/customAdapters.ts
@@ -1,7 +1,7 @@
 import { Address } from "viem";
 
 export const POPPIE_EULER_ADAPTER_CONTRACT_NAME = "PoppieEulerAdapter";
-export const POPPIE_CUSTOM_ADAPTER_DISPLAY_NAME = "Poppie Custom Adaptor";
+export const POPPIE_CUSTOM_ADAPTER_DISPLAY_NAME = "Poppie";
 export const POPPIE_EULER_ADAPTER_METADATA_HASH =
   "0x1220fc8e0ecc29a1ce0659055e968dfae362c42c436e53586f6c311d3f327018c513";
 
